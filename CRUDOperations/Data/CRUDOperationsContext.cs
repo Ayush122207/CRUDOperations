c92c2b9088beaf02fe51c8bf64733b327350081c
ritss <ritss@RITIKA>
2025-07-03 14:55:45 +0530

    Add project files.

---
+++ CRUDOperations/Data/CRUDOperationsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDOperations.Models;
+
+namespace CRUDOperations.Data
+{
+    public class CRUDOperationsContext : DbContext
+    {
+        public CRUDOperationsContext (DbContextOptions<CRUDOperationsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUDOperations.Models.Employees> Employees { get; set; } = default!;
+    }
+}
---
+++ CRUDOperations/Models/Employees.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDOperations.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? EmployeeFirstName { get; set; }
+        [Required]
+        public string? EmployeeLastName { get; set; }
+        [Required]
+        public DateTime? Birthday { get; set; }
+        public string? Address { get; set; }
+    }
+}
